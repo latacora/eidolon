ce446dd39885f7db7ac0979f8404022cc4e51475
Laurens Van Houtven (lvh) <lvh@latacora.com>
2019-04-11 21:51:31 -0500

    Initial stab at implementation

--- src/eidolon/core.clj
+++ src/eidolon/core.clj
@@ -1,1 +1,77 @@
-(ns eidolon.core)
+(ns eidolon.core
+  (:require [com.rpl.specter :as sr]))
+
+(def INDEXED
+  "A path that visits v and collects k in [[k v], ...].
+
+  This is useful if you want to collect a path to something, see [[path-walker]]
+  and [[path-finder]]. "
+  [sr/ALL (sr/collect-one sr/FIRST) sr/LAST])
+
+(def INDEXED-SEQ
+  "A selector that visits all elements of a seq, and collects their indices.
+
+  This is useful if you want to collect a path to something, see [[path-walker]]
+  and [[path-finder]]."
+  [(sr/view #(map-indexed vector %)) INDEXED])
+
+(def path-finder
+  "Finds the first entry matching `pred` in a deeply nested structure of maps
+  and vectors, and collects the path on the way there."
+  (sr/recursive-path
+   [term-pred] p
+   (sr/cond-path
+    (sr/pred term-pred) sr/STAY
+    map? [INDEXED p]
+    coll? [INDEXED-SEQ p])))
+
+(def path-walker
+  "Walks the first entry matching `pred` in a deeply nested structure of maps
+  and vectors, and collects the path on the way there.
+
+  This will do depth-first search. This is important, because if you use it with
+  a transform, you may be modifying the tree in a way that invalidates the
+  previously-collected path.
+
+  Like [[path-finder]], but will recurse into matching entries. Hence, also
+  takes a recurse path to tell it how to continue recursing on matches. For
+  example, if you're matching a vector, you may want [[sr/ALL]]; if you're
+  matching a map, you may want [[sr/MAP-VALS]]. Of course, those wouldn't
+  collect that part of the path, so you might want [[INDEXED]]
+  or [[INDEXED-SEQ]] instead."
+  (sr/recursive-path
+   [term-pred recurse-path] p
+   (sr/cond-path
+    (sr/pred term-pred) (sr/continue-then-stay recurse-path p)
+    map? [INDEXED p]
+    coll? [INDEXED-SEQ p])))
+
+(def NESTED
+  "A navigator for junctions in nested data structures."
+  (sr/recursive-path
+   [] p
+   (sr/cond-path
+    map? (sr/continue-then-stay sr/MAP-VALS p)
+    coll? (sr/continue-then-stay sr/ALL p))))
+
+(def NESTED-PATHS
+  "Like [[NESTED]], but collects the path along the way."
+  (sr/recursive-path
+   [] p
+   (sr/cond-path
+    map? [INDEXED p]
+    coll? [INDEXED-SEQ p]
+    sr/STAY sr/STAY)))
+
+(def TREE-KEYS
+  "A navigator for all of the map keys in a nested tree."
+  (sr/comp-paths NESTED map? sr/MAP-KEYS))
+
+(def TREE-LEAVES
+  "A navigator for all of the leaves in a nested tree."
+  (sr/recursive-path
+   [] p
+   (sr/cond-path
+    map? [sr/MAP-VALS p]
+    coll? [sr/ALL p]
+    sr/STAY sr/STAY)))
--- test/eidolon/core_test.clj
+++ test/eidolon/core_test.clj
@@ -1,7 +1,67 @@
 (ns eidolon.core-test
-  (:require [clojure.test :refer :all]
-            [eidolon.core :refer :all]))
+  (:require [clojure.test :as t]
+            [eidolon.core :as e]
+            [com.rpl.specter :as sr]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def finder-sample-haystack
+  {:a {:b {:c ['needle {:d ['second-needle]}]}
+       :x :nothing
+       :y :even-less}})
+
+(t/deftest path-finder-walker-tests
+  (t/is (= [[:a :b :c ['needle {:d ['second-needle]}]]]
+           (sr/select (e/path-finder vector?) finder-sample-haystack))
+        "path-finder finds the first needle, doesn't recurse")
+  (t/is (= [[:a :b :c 1 :d ['second-needle]]
+            [:a :b :c ['needle {:d ['second-needle]}]]]
+           (sr/select (e/path-walker vector? e/INDEXED-SEQ) finder-sample-haystack))
+        "path-walker finds the deepest needle first, then the shallow needle"))
+
+(t/deftest indexed-tests
+  (t/is (= [[:a :b] [:c [:d]]]
+           (sr/select [e/INDEXED] {:a :b :c [:d]}))))
+
+(t/deftest indexed-seq-tests
+  (let [xs [:a :b :c :d]]
+    (t/is (= (map-indexed vector xs)
+             (sr/select [e/INDEXED-SEQ] xs)))))
+
+(def simple-tree
+  {:a 1})
+
+(def tree-with-strings-in-it
+  {:a "Cooking MC's like a pound of bacon"})
+
+(def nested-tree
+  {:a {:b {:c 0
+           :d 1}
+       :e 2
+       :f #{3 4}
+       :g {:h 5 :i 6}}})
+
+(def nested-colls-tree
+  "A tree with values nested inside seqable things (sets, vecs...)"
+  {:a
+   [0 1
+    {:b
+     {:c 2
+      :d #{3 {:e 4
+              :f '(5 {:g 6})}}}}]})
+
+(t/deftest tree-keys-tests
+  (t/is (= [:a] (sr/select e/TREE-KEYS simple-tree)))
+  (t/is (= [:a] (sr/select e/TREE-KEYS tree-with-strings-in-it)))
+  (t/is (= #{:a :b :c :d :e :f :g :h :i}
+           (set (sr/select e/TREE-KEYS nested-tree))))
+  (t/is (= #{:a :b :c :d :e :f :g}
+           (set (sr/select e/TREE-KEYS nested-colls-tree)))))
+
+(t/deftest tree-leaves-tests
+  (t/is (= [1]
+           (sr/select e/TREE-LEAVES simple-tree)))
+  (t/is (= ["Cooking MC's like a pound of bacon"]
+           (sr/select e/TREE-LEAVES tree-with-strings-in-it)))
+  (t/is (= (set (range 7))
+           (set (sr/select e/TREE-LEAVES nested-tree))))
+  (t/is (= (set (range 7))
+           (set (sr/select e/TREE-LEAVES nested-colls-tree)))))
