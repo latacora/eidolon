2855032ca74265d7e13c91620a781f880c6bdab9
Laurens Van Houtven (lvh) <lvh@latacora.com>
2019-04-17 21:52:42 -0500

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject eidolon "0.1.0-SNAPSHOT"
+(defproject eidolon "0.1.0"
   :description "A collection of specter navigators"
   :url "https://github.com/latacora/eidolon"
   :license {:name "EPL-2.0"
