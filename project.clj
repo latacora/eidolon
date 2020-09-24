51ab2fde1696b2c180c6713ce21e04fa147925b2
Lizzie Dixon <lizzie@latacora.com>
2020-09-24 16:06:29 -0700

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject eidolon "0.2.0-SNAPSHOT"
+(defproject eidolon "0.2.0"
   :description "A collection of specter navigators"
   :url "https://github.com/latacora/eidolon"
   :license {:name "EPL-2.0"
